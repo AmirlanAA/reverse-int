98e149bc0bb3d00d6ec25d835e0a44db5ba481b0
@amirlanaa <amirlan.aimukhan@gmail.com>
2022-07-04 21:38:16 +0600

    feat: code to reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(object) {
+   //return object.split("").reverse().join("");
+   object = String(object);
+   let result = "";
+   for (let i = 0; i < object.length; i++) {
+      result = `${object[i]}${result}`;
+   }
+   result = parseInt(result);
+   return result;
 }
+///1 variant with charAt and substring
